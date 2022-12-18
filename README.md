5e1bf8f743d77f95fc700bd7b2c73bb51753f37d
AdMarom <adenamarom@gmail.com>
2022-12-17 18:24:21 -0800

    Successfully called weather API & added CSS Bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Weather-Application
 A website where a use can search a city &amp; get the current and future weather forecast using the open weather map api
+
+Resources:
+API Key 27da570459e0dbef2f6562c7c304faf3
+Open Weather Map: https://openweathermap.org/forecast5
+
---
+++ script.js
@@ -0,0 +1,44 @@
+
+//This will fetch latitude and longitude by city
+function getApi() {
+    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=27da570459e0dbef2f6562c7c304faf3";
+    fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        getWeather(data[0]);
+
+//This will fetch weather by longitude and latitude
+        function getWeather () {
+
+            console.log(data[0]);
+
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+
+            console.log(lat);
+            console.log(lon);
+
+            var requestNewUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=27da570459e0dbef2f6562c7c304faf3";
+
+            fetch(requestNewUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+    })
+    
+
+            
+
+            
+        }
+  })
+  
+
+}
+
+getApi()
